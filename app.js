5f1b4a35195ada14fc8ada74aa723ba45a245bc4
VishalBibe2001 <96763617+VishalBibe2001@users.noreply.github.com>
2021-12-28 23:35:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ hamburger.addEventListener('click',()=> {
 });
 document.addEventListener('scroll',() => {
     var scroll_position = window.scrollY;
-    if (scroll_position > 2) {
+    if (scroll_position > 1) {
         header.style.backgroundColor = "#29323c";
     }
      else {
